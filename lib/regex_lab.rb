d281a4c217db5234c6b456ad51d7ab9618b53ba1
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-15 06:19:32 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -11,11 +11,7 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-   if text.match(/\w{5}/)==nil
-     return false
-   else
-     return true
-   end
+   text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
