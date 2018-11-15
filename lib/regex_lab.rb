27b79a7b858bfcfc8611c51a830ee3de39eb973c
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-15 06:11:59 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if text.match(/^un/)==nil
+  if text.match(/(^un)(ing$)/)==nil
     return false
   else
     return true
